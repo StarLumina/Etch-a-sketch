af726940705683da059f58006b5e2ee8b6aa9f39
StarLuminary <paulalexism@gmail.com>
2025-06-27 23:19:01 -0600

    Finish base project

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const sontainer = document.getElementById(container)
+const container = document.getElementById("container")
+const remove = document.getElementById("clearGrid")
+const resize = document.getElementById("resizeGrid")
+let gridSide = 32
+
+createDivs(gridSide)
 
 function createDivs(side) {
     const totalDivs = side*side
@@ -12,17 +17,33 @@ function createDivs(side) {
 
 function removeChildren(){
     container.innerHTML = ""
+    createDivs(gridSide)
+}
+
+function getSide(){
+    let tempGrid=parseInt(prompt())
+    if ((Number.isInteger(tempGrid))&&(tempGrid<100)){
+        gridSide=tempGrid
+    } else {
+        alert("Please enter a valid number under 100")
+    }
 }
 
 container.addEventListener("mouseover",event=>{
     event.target.style.backgroundColor="black"
     // container.style.backgroundColor="white"
 })
+
 container.addEventListener("mouseover",()=>{
     // event.target.style.backgroundColor="black"
     container.style.backgroundColor="white"
 })
 
+remove.addEventListener("click",removeChildren)
+resize.addEventListener("click",()=>{
+    getSide()
+    removeChildren()
+})
 
 
-createDivs(50)
+
