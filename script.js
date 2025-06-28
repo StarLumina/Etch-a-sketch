bd02cd93b9125a0114245391f56d5aa08f4a38a4
StarLuminary <paulalexism@gmail.com>
2025-06-27 21:31:46 -0600

    Fix event related bugs
    Now the container doesnt change color and the divs are apropiately distributed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sontainer = document.getElementById(container)
 
 function createDivs(side) {
     const totalDivs = side*side
-    const sizeOfDivs=920/side
+    const sizeOfDivs=960/side
     for (i=0;i<totalDivs;i++){
         let newDiv = document.createElement('div')
         newDiv.style.width=`${sizeOfDivs}px`
@@ -10,4 +10,19 @@ function createDivs(side) {
     }
 }
 
-createDivs(16)
+function removeChildren(){
+    container.innerHTML = ""
+}
+
+container.addEventListener("mouseover",event=>{
+    event.target.style.backgroundColor="black"
+    // container.style.backgroundColor="white"
+})
+container.addEventListener("mouseover",()=>{
+    // event.target.style.backgroundColor="black"
+    container.style.backgroundColor="white"
+})
+
+
+
+createDivs(50)
